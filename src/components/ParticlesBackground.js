04327c5c0cea3ef3d0bfdf6581112e4a06c37206
faiz-edstem <faize.khan@edstem.com>
2022-10-22 15:28:23 +0530

    added particles

---
+++ src/components/ParticlesBackground.js
@@ -0,0 +1,30 @@
+import React, { useCallback } from 'react'
+import Particles from 'react-tsparticles'
+import { loadFull } from "tsparticles";
+import colors from './colors.config';
+import fireworks from './fireworks.config';
+import confetti from './confetti.config';
+
+const ParticlesBackground = () => {
+    const particlesInit = useCallback(async (engine) => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container) => {
+        await console.log(container);
+    }, []);
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={colors}
+        />
+    );
+};
+
+export default ParticlesBackground
