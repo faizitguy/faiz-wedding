338506f187da487f7621bbf64c396a0d029d083e
faiz-edstem <faize.khan@edstem.com>
2022-10-26 09:22:04 +0530

    integrated google analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import CountDown from './components/countdown/Countdown';
 import ParticlesBackground from './components/ParticlesBackground';
 import Timeline from './components/timeline';
 import Invitation from './components/invitation/Invitation';
+import ReactGa from 'react-ga';
 
 function App() {
+  useEffect(() => {
+    ReactGa.initialize('G-HPGQ206E6C');
+    // To Report page view
+    ReactGa.pageview('/');
+  }, []);
   return (
     <div>
       <ParticlesBackground />
